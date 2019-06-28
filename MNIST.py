72b3608afe1c6dd947f8a4eaf01aded08794d8da
Olav Opstad <Olav.Opstad@norgesgruppen.no>
2019-06-28 14:53:03 +0200

    ye

--- MNIST.py
+++ MNIST.py
@@ -1,5 +1,8 @@
 import numpy as np
 import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
 
 train_images = mnist.train_images()
 train_labels = mnist.train_labels()
@@ -16,3 +19,25 @@ test_images = test_images.reshape((-1, 784))
 
 print(train_images.shape) # (60000, 784)
 print(test_images.shape)  # (10000, 784)
+
+#build a model
+model = Sequential([
+  Dense(64, activation='relu', input_shape=(784,)),
+  Dense(64, activation='relu'),
+  Dense(10, activation='softmax'),
+])
+
+#compile the model
+model.compile(
+  optimizer='adam',
+  loss='categorical_crossentropy',
+  metrics=['accuracy'],
+)
+
+# train the model
+model.fit(
+  train_images, # training data
+  train_labels, # training targets
+  epochs=5,
+  batch_size=32,
+)
